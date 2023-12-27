5cb035ca7a7805eb79d1fd4741438db6d79b479f
MM <michele.makiki@gmail.com>
2023-12-27 18:46:46 +0200

    anecdotes and hooks step1

--- routed-anecdotes/src/App.jsx
+++ routed-anecdotes/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useField } from './hooks'
 
 import {
   Routes,
@@ -81,22 +82,27 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('content')
+  const author = useField('author')
+  const info = useField('info')
   const navigate = useNavigate()
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
+    console.log(content)
+    console.log(author)
+    console.log(info)
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
+    
   }
+
 
   return (
     <div>
@@ -104,15 +110,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e) => setInfo(e.target.value)} />
+          <input {...info}/>
         </div>
         <button>create</button>
       </form>
@@ -138,7 +144,6 @@ const App = () => {
       id: 2
     }
   ])
-  const [notification, setNotification] = useState('')
   const [info, setInfo] = useState({ message: null })
 
   const notifyWith = (message) => {
